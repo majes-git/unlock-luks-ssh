41766a3437a4078938b3143073b303f5b1d47592
Mathias Jeschke <mj@majes.de>
2023-01-17 23:55:32 +0100

    Extended sanity checks and provide more meaningful logging

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ import sys
 import time
 
 DELAY = 60
+NO_REPEAT = False
 
 
 def log(*messages):
     print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), *messages)
     sys.stdout.flush()
 
+def log_not_repeat(*messages):
+    if not NO_REPEAT:
+        log(*messages)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -26,22 +30,45 @@ def parse_arguments():
 
 
 def unlock_system(hostname, port, passphrase):
+    global NO_REPEAT
     ssh_options = '-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
     ssh_user = 'root'
     child = pexpect.spawn('/usr/bin/ssh {} -p {} {}@{}'.format(
         ssh_options, port, ssh_user, hostname))
 
-    log('Trying to unlock system...')
-    selection = child.expect(
-        ['Permission denied', 'Please unlock disk'], timeout=5)
-    if selection == 0:
-        log('SSH login with key failed. Please check if public key was installed properly!')
-        child.kill(0)
-    elif selection == 1:
-        child.sendline(passphrase)
-        log('Unlock was successful.')
-        child.expect('set up successfully\r\n')
-
+    log_not_repeat('Trying to unlock system...')
+    try:
+        selection = child.expect(
+            ['Permission denied',
+             'Timeout reached while waiting for askpass',
+             'Please unlock disk'], timeout=30)
+        if selection == 0:
+            NO_REPEAT = False
+            log('SSH login with key failed.',
+                'Please check if public key was installed properly!')
+            child.kill(0)
+        elif selection == 1:
+            log_not_repeat('Askpass is not running.',
+                           'The target system seems to be stuck.',
+                           'Try to power cyle.')
+            NO_REPEAT = True
+            child.kill(0)
+        elif selection == 2:
+            NO_REPEAT = False
+            child.sendline(passphrase)
+            selection = child.expect(
+                ['cryptsetup: cryptsetup failed, bad password or options?',
+                 'set up successfully\r\n'])
+            if selection == 0:
+                log('The configured passphrase seems to be wrong. Exiting.')
+                child.kill(0)
+                sys.exit(1)
+            elif selection == 1:
+                log('Unlock was successful.')
+    except pexpect.exceptions.TIMEOUT:
+        log('SSH seems to be running, but the response is unexpected!')
+    except pexpect.exceptions.EOF:
+        log('Unexpected end of file for SSH connection.')
 
 def system_is_waiting_for_passphrase(hostname, port=22):
     # try to open a socket in order to check if ssh listening at hostname/port
